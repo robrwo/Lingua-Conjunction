60587d9e6d421f5a25c1df32a972d99cffb52335
Robert Rothenberg <rrwo@cpan.org>
2018-01-28 21:38:44 +0000

    Commit Changes and bump $VERSION

--- lib/Lingua/Conjunction.pm
+++ lib/Lingua/Conjunction.pm
@@ -13,7 +13,7 @@ use Exporter qw/ import /;
 our @EXPORT    = qw( conjunction );
 our @EXPORT_OK = @EXPORT;
 
-our $VERSION = 'v2.1.0';
+our $VERSION = 'v2.1.1';
 
 # Language-specific definitions (these may not be correct, and certainly
 # they are not complete... E-mail corrections and additions to the author
